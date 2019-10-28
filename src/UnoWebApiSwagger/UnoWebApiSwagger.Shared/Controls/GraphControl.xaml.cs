bbe03a9280b3748e698a0f77f6fb208d6ae3918a
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-10-28 15:42:22 +0100

    Added graph (Skia)

---
+++ src/UnoWebApiSwagger/UnoWebApiSwagger.Shared/Controls/GraphControl.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using SkiaSharp;
+using SkiaSharp.Views.UWP;
+
+namespace SkiaSharpTest.Shared
+{
+    public sealed partial class GraphControl
+    {
+        private const int PreviewPointCount = 3;
+
+
+        public GraphControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private static readonly Random Rnd = new Random();
+
+        void OnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+            
+            var points = GetPoints();
+            var path = new SKPath();
+            var translatedPoints = points.Select(tuple => ((float)(tuple.Item1 * info.Width), (float)(info.Height - tuple.Item2 * info.Height))).ToList();
+
+            var (startX, startY) = translatedPoints.First();
+
+            path.MoveTo(startX, startY);
+            foreach (var (x, y) in translatedPoints)
+            {
+                path.LineTo(x, y);
+            }
+
+            var strokePaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Red,
+                StrokeWidth = 1,
+                FilterQuality = SKFilterQuality.High,
+                IsAntialias = true,
+            };
+
+            canvas.DrawPath(path, strokePaint);
+        }
+
+        private IEnumerable<(double, double)> GetPoints()
+        {
+            if (Values == null)
+            {
+                return Enumerable.Range(0, PreviewPointCount)
+                    .Select(x => ((double) x / (PreviewPointCount - 1), Rnd.NextDouble())).ToList();
+            }
+
+            var doubles = Values.Cast<double>().ToList();
+            var count = doubles.Count;
+            return Enumerable.Range(0, count).Zip(doubles, (n, y) => ((double) n / (count - 1), y))
+                .ToList();
+        }
+
+        public static readonly DependencyProperty ValuesProperty = DependencyProperty.Register(
+            "Values", typeof(IEnumerable), typeof(GraphControl), new PropertyMetadata(default(IEnumerable)));
+
+        public IEnumerable Values
+        {
+            get { return (IEnumerable) GetValue(ValuesProperty); }
+            set { SetValue(ValuesProperty, value); }
+        }
+    }
+}
---
+++ src/UnoWebApiSwagger/UnoWebApiSwagger.Shared/CurrencyToValuesConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+using UnoWebApiSwagger.WebApiClient;
+
+namespace UnoWebApiSwagger.Shared
+{
+    class CurrencyToValuesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Currency c)
+            {
+                var values = new[] {c.SpotRate, c.SpotWeek}.Select(x => (double)x).ToList();
+                var offset = values.Min();
+
+                var deOffset = values.Select(x => x - offset).ToList();
+                var max = deOffset.Max();
+
+                return deOffset.Select(x =>
+                {
+                    var d = x / max;
+                    return double.IsNaN(d) ? 0d : d;
+                }).ToList();
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
