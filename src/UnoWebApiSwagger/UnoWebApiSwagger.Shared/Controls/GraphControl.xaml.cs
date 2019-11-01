f460465f9725ad14663e80e83a49c3058a48292f
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-11-01 20:55:35 +0100

    General fixes

--- src/UnoWebApiSwagger/UnoWebApiSwagger.Shared/Controls/GraphControl.xaml.cs
+++ src/UnoWebApiSwagger/UnoWebApiSwagger.Shared/Controls/GraphControl.xaml.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Windows.Foundation;
 using Windows.UI.Xaml;
 using SkiaSharp;
 using SkiaSharp.Views.UWP;
-using Uno.Extensions;
+using UnoWebApiSwagger.Controls;
 
 namespace UnoWebApiSwagger.Shared
 {
@@ -13,48 +14,120 @@ namespace UnoWebApiSwagger.Shared
     {
         private const int PreviewPointCount = 3;
 
+        private static readonly Random Rnd = new Random();
+
+        public static readonly DependencyProperty ValuesProperty = DependencyProperty.Register(
+            "Values", typeof(IEnumerable), typeof(GraphControl), new PropertyMetadata(default(IEnumerable)));
+
+        private readonly SKPaint _strokePaint;
+
+        private readonly SKPaint _textPaint;
 
         public GraphControl()
         {
-            this.InitializeComponent();
-        }
+            InitializeComponent();
 
-        private static readonly Random Rnd = new Random();
-
-        void OnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
-        {
-            SKImageInfo info = args.Info;
-            SKSurface surface = args.Surface;
-            SKCanvas canvas = surface.Canvas;
-
-            canvas.Clear();
-
-            var points = ToViewPort(GetPoints().ToList(), info.Width, info.Height).ToList();
-
-            var path = new SKPath();
-            var translatedPoints = points.Select(tuple => ((float)tuple.Item1, (float)(info.Height - tuple.Item2))).ToList();
-
-            var (startX, startY) = translatedPoints.First();
-
-            path.MoveTo(startX, startY);
-            foreach (var (x, y) in translatedPoints)
+            _textPaint = new SKPaint
             {
-                path.LineTo(x, y);
-            }
+                Color = SKColors.Black,
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill,
+                TextAlign = SKTextAlign.Center,
+                TextSize = 12
+            };
 
-            var strokePaint = new SKPaint
+            _strokePaint = new SKPaint
             {
                 Style = SKPaintStyle.Stroke,
                 Color = SKColors.Red,
                 StrokeWidth = 1,
                 FilterQuality = SKFilterQuality.High,
-                IsAntialias = true,
+                IsAntialias = true
             };
-
-            canvas.DrawPath(path, strokePaint);
         }
 
-        private IEnumerable<(double, double)> ToViewPort(IList<(double, double)> points, int viewportWidth, int viewportHeight)
+
+        public IEnumerable Values
+        {
+            get => (IEnumerable)GetValue(ValuesProperty);
+            set => SetValue(ValuesProperty, value);
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            var info = args.Info;
+            var surface = args.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            var points = ToViewPort(GetPoints().ToList(), info.Width, info.Height).ToList();
+
+            PlotPoints(points, canvas);
+            DrawLabels(points, canvas, args.Info.Width);
+        }
+
+        private void DrawLabels(IEnumerable<PlotPoint> points, SKCanvas canvas, int width)
+        {
+            foreach (var plotPoint in points)
+            {
+                DrawLabel(canvas, plotPoint, width);
+            }
+        }
+
+        private void PlotPoints(IReadOnlyCollection<PlotPoint> points, SKCanvas canvas)
+        {
+            var path = new SKPath();
+
+            var first = points.First();
+
+            path.MoveTo((float)first.Translated.X, (float)first.Translated.Y);
+            foreach (var plotPoint in points)
+            {
+                Plot(path, plotPoint);
+            }
+
+            canvas.DrawPath(path, _strokePaint);
+        }
+
+        private void DrawLabel(SKCanvas canvas, PlotPoint plotPoint, int width)
+        {
+            var text = plotPoint.Original.Y.ToString("F0");
+            var vertOffset = plotPoint.Translated.Y < _textPaint.TextSize ? _textPaint.TextSize : 0;
+
+            var horzOffset = GetHorzOffset(text, plotPoint, width);
+
+            var point = new SKPoint((float)(plotPoint.Translated.X + horzOffset), (float)plotPoint.Translated.Y + vertOffset);
+            canvas.DrawText(text, point, _textPaint);
+        }
+
+        private double GetHorzOffset(string text, PlotPoint plotPoint, int viewportWidth)
+        {
+            var width = _textPaint.MeasureText(text);
+            var halfWidth = width / 2;
+
+            if (plotPoint.Translated.X < halfWidth)
+            {
+                return halfWidth;
+            }
+
+            if (plotPoint.Translated.X + halfWidth > viewportWidth)
+            {
+                return -halfWidth;
+            }
+
+            return 0;
+
+        }
+
+        private void Plot(SKPath path, PlotPoint plotPoint)
+        {
+            var x = (float)plotPoint.Translated.X;
+            var y = (float)plotPoint.Translated.Y;
+            path.LineTo(x, y);
+        }
+
+        private IEnumerable<PlotPoint> ToViewPort(IList<(double, double)> points, int viewportWidth, int viewportHeight)
         {
             var maxY = points.Max(tuple => tuple.Item2);
             var minY = points.Min(tuple => tuple.Item2);
@@ -69,7 +142,7 @@ namespace UnoWebApiSwagger.Shared
                 var (origX, origY) = tuple;
                 var x = origX * viewportWidth / diffX;
                 var y = (origY - minY) * viewportHeight / diffY;
-                return (x, y);
+                return new PlotPoint(new Point(origX, origY), new Point(x, viewportHeight - y));
             });
 
             return translated;
@@ -84,15 +157,6 @@ namespace UnoWebApiSwagger.Shared
             }
 
             return Values.Cast<(double, double)>().ToList();
-        }
-
-        public static readonly DependencyProperty ValuesProperty = DependencyProperty.Register(
-            "Values", typeof(IEnumerable), typeof(GraphControl), new PropertyMetadata(default(IEnumerable)));
-
-        public IEnumerable Values
-        {
-            get { return (IEnumerable)GetValue(ValuesProperty); }
-            set { SetValue(ValuesProperty, value); }
         }
     }
 }
---
+++ src/UnoWebApiSwagger/UnoWebApiSwagger.Shared/Controls/PlotPoint.cs
@@ -0,0 +1,17 @@
+﻿using Windows.Foundation;
+
+namespace UnoWebApiSwagger.Controls
+{
+    internal class PlotPoint
+    {
+        public PlotPoint(Point original, Point translated)
+        {
+            Original = original;
+            Translated = translated;
+        }
+
+        public Point Original { get; }
+
+        public Point Translated { get; }
+    }
+}
